4bcc50c4ae93bc60594cabb857daa77d0530aa66
Bart Cerneels <bart@theapplitree.be>
2022-07-11 15:45:36 +0200

    REPL mode does not need GUI loop
    Just show text on screen and exit.

--- packages/apps/repl.py
+++ packages/apps/repl.py
@@ -1,37 +1,22 @@
 import hardware_setup
-from gui.core.ugui import Screen, ssd, Window
-from gui.widgets import Label, Button, CloseButton
 from gui.core.writer import CWriter
 
 # Font for CWriter
 import gui.fonts.font10 as font
 from gui.core.colors import *
 
-import system
-
-import uasyncio as asyncio
+ssd = hardware_setup.ssd
 
 wri = CWriter(ssd, font, YELLOW, BLACK, verbose=False)
 
-class BaseScreen(Screen):
-    def __init__(self):
-        super().__init__()
-
-        row = (ssd.height//2)
-        col = 2
-        l = Label(wri, row, col, text='REPL running on USB serial.')
-        print(l.row, l.width)
-        row = ssd.height - 30
-        print(Label(wri, row, 10, 'Hold boot 5 seconds to recover.').width)
-        CloseButton(wri)
-
-async def shutdown():
-    await asyncio.sleep(5)
-    BaseScreen.shutdown()
+display = hardware_setup.disp
 
 def run():
     print("REPL is running.")
-    asyncio.create_task(shutdown())
-    Screen.change(BaseScreen)
+    display.print_centred(wri, ssd.width//2, ssd.height//2, 'REPL running on USB serial.')
+
+    display.print_centred(wri, ssd.width//2, ssd.height-20, 'Hold boot 5 seconds to recover.')
+
+    ssd.show()
 
 run()
